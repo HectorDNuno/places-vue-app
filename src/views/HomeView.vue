a26ac5f118ab3c86fdfbfa2be0c6c58ef3403766
hector <hectornuno1994@gmail.com>
2022-04-12 14:09:47 -0700

    adds ability to see places

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,29 @@
-<template>
-  <div class="home">
-    <h1>{{ message }}</h1>
-  </div>
-</template>
-
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      places: {},
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("http://localhost:3000/places").then((response) => {
+      console.log("all places", response.data);
+      this.places = response.data;
+    });
+  },
   methods: {},
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>All your places!</h1>
+
+    <div v-for="place in places" :key="place.id">
+      <p>Name: {{ place.name }}</p>
+      <p>Address: {{ place.address }}</p>
+    </div>
+  </div>
+</template>
